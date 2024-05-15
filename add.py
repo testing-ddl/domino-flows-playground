1c33616835cdfdca5c6702f40329086d99540ad5
John Wu <john.wu@dominodatalab.com>
2024-05-15 22:25:38 +0000

    Updates to hello world example

--- add.py
+++ add.py
@@ -1,20 +1,12 @@
-def read_input(input_name):
-    input_location = f"/workflow/inputs/{input_name}"
-    with open(input_location, "r") as file:
-        return file.read()
-
-def write_output(output_name, content):
-    output_location = f"/workflow/outputs/{output_name}"
-    with open(output_location, 'w') as file:
-        file.write(content)
+from pathlib import Path
 
 # Read inputs
-a = int(read_input("first_value"))
-b = int(read_input("second_value"))
+a = Path("/workflow/inputs/first_value").read_text()
+b = Path("/workflow/inputs/second_value").read_text()
 
 # Calculate sum
-sum = a + b
+sum = int(a) + int(b)
 print(f"The sum of {a} + {b} is {sum}")
 
 # Write output
-write_output("sum", str(sum))
+Path("/workflow/inputs/sum").write_text(str(sum))
--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 import os
 from flytekit import workflow
-from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
 
 @workflow
 def hello_workflow(a: int, b: int) -> float:
@@ -9,7 +9,7 @@ def hello_workflow(a: int, b: int) -> float:
     # Create first task 
     add_job = DominoJobTask(
         name='Add numbers',
-        domino_job_config=DominoJobConfig(ApiKey=os.environ.get('DOMINO_USER_API_KEY'), MainRepoGitRef=GitRef(Type="head"),Command="python add.py"),
+        domino_job_config=DominoJobConfig(Command="python add.py"),
         inputs={'first_value': int, 'second_value': int},
         outputs={'sum': int},
         use_latest=True
@@ -19,15 +19,36 @@ def hello_workflow(a: int, b: int) -> float:
     # Create second task 
     sqrt_job = DominoJobTask(
         name='Square root',
-        domino_job_config=DominoJobConfig(
-            ApiKey=os.environ.get('DOMINO_USER_API_KEY'),
-            MainRepoGitRef=GitRef(Type="head"),
-            Command="python sqrt.py"
-        ),
+        domino_job_config=DominoJobConfig(Command="python sqrt.py"),
         inputs={'value': int},
         outputs={'sqrt': float},
         use_latest=True
     )
     sqrt = sqrt_job(value=sum)
-    
+
+    return sqrt
+
+@workflow
+def hello_workflow_dev(a: int, b: int) -> float:
+
+    # Create first task 
+    add_job = DominoJobTask(
+        name='Add numbers',
+        domino_job_config=DominoJobConfig(ApiKey=os.environ.get('DOMINO_USER_API_KEY'), MainRepoGitRef=GitRef(Type="head"), Command="python add.py"),
+        inputs={'first_value': int, 'second_value': int},
+        outputs={'sum': int},
+        use_latest=True
+    )
+    sum = add_job(first_value=a, second_value=b)
+
+    # Create second task 
+    sqrt_job = DominoJobTask(
+        name='Square root',
+        domino_job_config=DominoJobConfig(ApiKey=os.environ.get('DOMINO_USER_API_KEY'), MainRepoGitRef=GitRef(Type="head"), Command="python sqrt.py"),
+        inputs={'value': int},
+        outputs={'sqrt': float},
+        use_latest=True
+    )
+    sqrt = sqrt_job(value=sum)
+
     return sqrt
