3939670722925b6dd6108cdcb724e0e239a3b38b
John Wu <john.wu@dominodatalab.com>
2024-05-15 22:34:30 +0000

    Updates to hello world example

--- sqrt.py
+++ sqrt.py
@@ -1,19 +1,11 @@
-def read_input(input_name):
-    input_location = f"/workflow/inputs/{input_name}"
-    with open(input_location, "r") as file:
-        return file.read()
-
-def write_output(output_name, content):
-    output_location = f"/workflow/outputs/{output_name}"
-    with open(output_location, 'w') as file:
-        file.write(content)
+from pathlib import Path
 
 # Read inputs
-value = int(read_input("value"))
+value = Path("/workflow/inputs/value").read_text()
 
 # Calculate square root
-sqrt = value ** 0.5
+sqrt = int(value) ** 0.5
 print(f"The square root of {value} is {sqrt}")
 
 # Write output
-write_output("sqrt", str(sqrt))
+Path("/workflow/outputs/sqrt").write_text(str(sqrt))
