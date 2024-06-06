999cc225eaaf9a79df57994f78eee49e438080b9
Rfqa 011 GBP <rfqa+011-gbp@dominodatalab.com>
2024-06-06 10:20:15 -0700

    Pushed from Domino: https://sw511runb.workbench-analyze-team-sandbox.domino.tech/workspace/rfqa-011-gbp/flows-test?executionId=6661eb954179da531ff3becd

--- workflow.py
+++ workflow.py
@@ -21,7 +21,7 @@ def training_workflow(data_path: str) -> FlyteFile:
     :return: The training results as a model
     """
 
-    data_prep_results = DominoTask(
+    data_prep_results = DominoJobTask(
         name="Prepare data",
         command="python /mnt/code/scripts/prep-data.py",
         environment="Data Prep Environment",
@@ -34,7 +34,7 @@ def training_workflow(data_path: str) -> FlyteFile:
         ]
     )
 
-    training_results = DominoTask(
+    training_results = DominoJobTask(
         name="Train model",
         command="python /mnt/code/scripts/train-model.py",
         environment="Training Environment",
@@ -88,7 +88,7 @@ def training_subworkflow(data_path: str) -> FlyteFile:
 @workflow
 def generate_types(): 
 
-    sce_types = DominoTask(
+    sce_types = DominoJobTask(
         name="Generate SCE Types",
         command="python /mnt/code/scripts/generate-sce-types.py",
         environment="Domino Standard Environment Py3.9 R4.3",
@@ -102,7 +102,7 @@ def generate_types():
         ]
     )
 
-    ml_types = DominoTask(
+    ml_types = DominoJobTask(
         name="Generate ML Types",
         command="python /mnt/code/scripts/generate-ml-types.py",
         environment="Domino Standard Environment Py3.9 R4.3",
@@ -150,32 +150,35 @@ def training_workflow_git(data_path: str) -> FlyteFile:
     :return: The training results as a model
     """
 
-    data_prep_results = DominoTask(
+    data_prep_results = DominoJobTask(
         name="Prepare data ",
-        command="python /mnt/code/scripts/prep-data.py",
+        domino_job_config=DominoJobConfig(MainRepoGitRef=GitRef(Type="head"),Command="python /mnt/code/scripts/prep-data.py"),
         environment="Data Prep Environment",
         hardware_tier="Small",
-        inputs=[
-            Input(name="data_path", type=str, value=data_path)
-        ],
-        outputs=[
-            Output(name="processed_data", type=FlyteFile)
-        ]
+        inputs={'data_path': str},
+        outputs={'processed_data': FlyteFile}
     )
 
-    training_results = DominoTask(
+    # add_task = DominoJobTask(
+    #     name='Add numbers',
+    #     domino_job_config=DominoJobConfig(MainRepoGitRef=GitRef(Type="head"),Command="python add.py"),
+    #     inputs={'first_value': int, 'second_value': int},
+    #     outputs={'sum': int},
+    #     use_latest=True
+    # )
+    # sum = add_task(first_value=a, second_value=b)
+
+    epochs = 10
+    batch_size = 32
+    processed_data = data_prep_results['processed_data']
+
+    training_results = DominoJobTask(
         name="Train model ",
         command="python /mnt/code/scripts/train-model.py",
         environment="Training Environment",
         hardware_tier="Medium",
-        inputs=[
-            Input(name="processed_data", type=FlyteFile, value=data_prep_results['processed_data']),
-            Input(name="epochs", type=int, value=10),
-            Input(name="batch_size", type=int, value=32)
-        ],
-        outputs=[
-            Output(name="model", type=FlyteFile)
-        ]
+        inputs={'processed_data': FlyteFile, 'epochs': int, 'batch_size': int},
+        outputs={'model': FlyteFile}
     )
 
     return training_results['model']
@@ -186,7 +189,7 @@ def training_workflow_nested(data_path: str):
 
     model = training_subworkflow(data_path=data_path)
 
-    results = DominoTask(
+    results = DominoJobTask(
         name="Final task",
         command="sleep 100",
         environment="Training Environment",
@@ -210,7 +213,7 @@ def training_workflow_mlflow():
     pyflyte run --remote workflow.py training_workflow_mlflow
     """
 
-    data_prep_results = DominoTask(
+    data_prep_results = DominoJobTask(
         name="Run experiment",
         command="python /mnt/code/scripts/experiment.py",
         environment="Training Environment",
