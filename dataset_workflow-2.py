f12b0e7bfb8eadffd5393751faaac0db5d15e6f4
Rfqa 011 GBP <rfqa+011-gbp@dominodatalab.com>
2024-08-16 17:33:55 -0700

    Pushed from Domino: https://s5112uat57703.train-sandbox.domino.tech/workspace/rfqa-011-gbp/flows-gbp?executionId=66bfdc5bd58a430eb1d2a43d

---
+++ dataset_workflow-2.py
@@ -0,0 +1,23 @@
+
+import os
+from flytekit import workflow
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, DatasetSnapshot
+
+# This test code assumes a project name of flows-gbp
+
+# pyflyte run --remote dataset_workflow-2.py dataset_workflow
+
+@workflow
+def dataset_workflow():
+
+ DominoJobTask(
+     name="My dataset workflow",
+     domino_job_config=DominoJobConfig(
+         Command="ls -lR /mnt/data/flows-gbp;ls -lR /mnt/data/flows-dfs",
+         DatasetSnapshots=[
+             DatasetSnapshot(Id="66bfdc2ad58a430eb1d2a43a", Name="flows-gbp", Version=1),
+             DatasetSnapshot(Id="66bfee8cd58a430eb1d2a48c", Name="flows-dfs", Version=3),
+         ],
+     ),
+     use_latest=True,
+ )()
