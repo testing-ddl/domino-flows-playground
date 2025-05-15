0b9e8d3c3afe716472e91362ac303adb7df35b61
Rfqa 011 GBP <rfqa+011-gbp@dominodatalab.com>
2025-05-14 17:04:44 -0700

    Pushed from Domino: https://sw610uat81467.workbench-analyze-team-sandbox.domino.tech/workspace/rfqa-011-gbp/flows-gbp?executionId=68250ecacdbdb620a71d429e

--- cache-wf.py
+++ cache-wf.py
@@ -64,45 +64,45 @@ def training_workflow_cache(data_path: str) -> final_outputs:
     This workflow accepts a path to a CSV for some initial input and simulates
     the processing of the data and usage of the processed data in a training job.
     To run this workflowp, execute the following line in the terminal
-    pyflyte run --remote cache-wf.py training_workflow --data_path /mnt/code/artifacts/data.csv
+    pyflyte run --remote cache-wf.py training_workflow_cache --data_path /mnt/code/artifacts/data.csv
     :param data_path: Path of the CSV file data
     :return: The training results as a model
     """
-
-    data_prep_job_config = DominoJobConfig(
-        Command="python /mnt/code/scripts/prep-data.py",
-        MainRepoGitRef=GitRef(Type="commitId", Value="2f1cb9bf696921f0")
-    )
-    data_prep_job = DominoJobTask(
-        name='Prepare Data',
-        domino_job_config=data_prep_job_config,
-        inputs={'data_path': str},
-        outputs={'processed_data': FlyteFile[TypeVar("csv")]},
+    
+    data_prep_results = run_domino_job_task(
+        flyte_task_name="Prepare data",
+        command="python /mnt/code/scripts/prep-data.py",
+        main_git_repo_ref=GitRef(Type="commitId", Value="9fc19c5d95e4a21a5b677ce4ac7896d301d831b9"),
         environment_name="Domino Core Environment",
-        # environment_name="Domino Standard Environment Py3.10 R4.4",
         hardware_tier_name="Small",
-        # use_latest=True,
+        inputs=[
+            Input(name="data_path", type=str, value=data_path)
+        ],
+        output_specs=[
+            Output(name="processed_data", type=FlyteFile[TypeVar("csv")])
+        ],
+        use_project_defaults_for_omitted=True,
         cache=True,
         cache_version="1.0"
     )
-    data_prep_results = data_prep_job(data_path=data_path)
 
-    training_job_config = DominoJobConfig(
-        Command="python /mnt/code/scripts/train-model.py",
-        MainRepoGitRef=GitRef(Type="commitId", Value="2f1cb9bf696921f0")
-    )
-    training_job = DominoJobTask(
-        name='Train model',
-        domino_job_config=training_job_config,
-        inputs={'processed_data': FlyteFile[TypeVar("csv")]},
-        outputs={'model': FlyteFile},
+    training_results = run_domino_job_task(
+        flyte_task_name="Train model",
+        command="python /mnt/code/scripts/train-model.py",
         environment_name="Domino Core Environment",
-        # environment_name="Domino Standard Environment Py3.10 R4.4",
+        main_git_repo_ref=GitRef(Type="commitId", Value="9fc19c5d95e4a21a5b677ce4ac7896d301d831b9"),
         hardware_tier_name="Small",
-        # use_latest=True,
+        inputs=[
+            Input(name="processed_data", type=FlyteFile[TypeVar("csv")], value=data_prep_results['processed_data']),
+            Input(name="epochs", type=int, value=10),
+            Input(name="batch_size", type=int, value=32)
+        ],
+        output_specs=[
+            Output(name="model", type=FlyteFile)
+        ],
+        use_project_defaults_for_omitted=True,
         cache=True,
         cache_version="1.0"
     )
-    training_results = training_job(processed_data=data_prep_results["processed_data"])
 
     return final_outputs(model=training_results['model'])
