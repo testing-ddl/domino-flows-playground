f108cbcad1c63a226e5105c21c3c3fb5c07b5b87
ddl-rliu <richard.liu@dominodatalab.com>
2025-05-23 15:21:05 -0700

    x

--- generate_and_run_workflow_for_dataset.sh
+++ generate_and_run_workflow_for_dataset.sh
@@ -1,2 +1,8 @@
-sed "s/DATASET_ID_TEMPLATE/$1/g" generate_artifacts.py > generate_artifacts_with_dataset_exports.py
+"""
+Helper script to run generate_artifacts.generate_artifacts_with_dataset_exports with the specified dataset.
+The script parses the dataset id from the input dataset URL, and creates a new workflow file with this dataset
+id injected into it via sed.
+"""
+DATASET_ID=$(basename "$1")
+sed "s/DATASET_ID_TEMPLATE/$DATASET_ID/g" generate_artifacts.py > generate_artifacts_with_dataset_exports.py
 pyflyte run --remote generate_artifacts_with_dataset_exports.py generate_artifacts_with_dataset_exports
