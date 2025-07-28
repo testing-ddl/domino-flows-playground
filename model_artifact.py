2504fca94ff12b182f78775a938ff3bc826b06ee
Rfqa 011 GBP <rfqa+011-gbp@dominodatalab.com>
2025-07-28 14:59:31 -0700

    Pushed from Domino: https://sw620uat90173.engineering-dev.domino.tech/workspace/rfqa-011-gbp/flows-gbp?executionId=6887f1a5a924353296cc4c8b

--- model_artifact.py
+++ model_artifact.py
@@ -1,21 +1,36 @@
-from typing import TypeVar
-from flytekitplugins.domino.helpers import Output, run_domino_job_task
-from flytekitplugins.domino.artifact import Artifact, MODEL
 from flytekit import workflow
 from flytekit.types.file import FlyteFile
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask
+from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
+from typing import TypeVar, NamedTuple
 
-ModelArtifact = Artifact("My Model", MODEL)
+# define the artifact name and type which may be REPORT, DATA or MODEL
+DataArtifact = Artifact(name="My Data", type=DATA)
+ModelArtifact = Artifact(name="My Model", type=MODEL)
 
-# pyflyte run --remote model_artifact.py single_model
+# pyflyte run --remote model_artifact.py training_workflow --data_path /mnt/code/artifacts
 
 @workflow
-def single_model() -> ModelArtifact.File(name="model.pkl"):
-    return run_domino_job_task(
-        flyte_task_name="Produce model",
-        command="produce_model.py",
-        output_specs=[
-            # name of the Output can differ from the name of the ArtifactFile
-            Output(name="my_model", type=FlyteFile[TypeVar("pkl")]),
-        ],
-        use_project_defaults_for_omitted=True,
+def training_workflow(data_path: str) -> ModelArtifact.File(name="model.pt"):
+
+    data_prep_job_config = DominoJobConfig(Command="python prep-data.py")
+    data_prep_job = DominoJobTask(
+        name='Prep data',
+        domino_job_config=data_prep_job_config,
+        inputs={'data_path': str},
+        outputs={'processed_data': DataArtifact.File(name="data.csv")},
+        use_latest=True
     )
+    data_prep_results = data_prep_job(data_path=data_path)
+
+    training_job_config = DominoJobConfig(Command="python train-model.py")
+    training_job = DominoJobTask(
+        name='Train model',
+        domino_job_config=training_job_config,
+        inputs={'processed_data': FlyteFile[TypeVar("csv")]},
+        outputs={'model': FlyteFile[TypeVar("pt")]},
+        use_latest=True
+    )
+    training_results = training_job(processed_data=data_prep_results["processed_data"])
+
+    return training_results["model"] # Final output is returned here
