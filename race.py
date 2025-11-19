f4194cc38d3d8a883109802ae4d44d9389702088
ddl-rliu <140021987+ddl-rliu@users.noreply.github.com>
2025-11-19 15:56:00 -0800

    Update race.py

--- race.py
+++ race.py
@@ -3,11 +3,11 @@ import time
 import random
 
 
-# current_time = datetime.now().strftime('%M:%S')
-# while True:
-#     if current_time.endswith("0:00") or current_time.endswith("5:00"):
-#         break
-#     time.sleep(0.01)
+current_time = datetime.now().strftime('%M:%S')
+while True:
+    if current_time.endswith("0:00") or current_time.endswith("5:00"):
+        break
+    time.sleep(0.01)
 
 with open("/mnt/artifacts/f_"+str(random.random())+"txt", "w") as f:
     f.write(str(random.random()))
