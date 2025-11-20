0eaa9a26afe4caf46c817213f13ac3dc3190b353
ddl-rliu <140021987+ddl-rliu@users.noreply.github.com>
2025-11-19 16:03:00 -0800

    Update race.py

--- race.py
+++ race.py
@@ -2,9 +2,8 @@ from datetime import datetime
 import time
 import random
 
-
-current_time = datetime.now().strftime('%M:%S')
 while True:
+    current_time = datetime.now().strftime('%M:%S')
     if current_time.endswith("0:00") or current_time.endswith("5:00"):
         break
     time.sleep(0.01)
